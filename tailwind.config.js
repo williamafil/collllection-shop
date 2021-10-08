ff9e4c80a3d45f289a904ab331e63a5acc900deb
William <will2b@gmail.com>
2021-10-08 01:34:50 -0700

    theme setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 module.exports = {
-  mode: 'jit',
+  mode: "jit",
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightOrange: {
+          50: "#fcf6ef",
+          100: "#f3edeb",
+          800: "#d36651",
+        },
+        dark: "#162317",
+        lightTeal: "#9ab1ac",
+      },
+      fontFamily: {
+        arsenal: "'Arsenal', 'sans-serif'",
+      },
+    },
   },
   variants: {
     extend: {},
