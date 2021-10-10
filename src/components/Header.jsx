0f384085ac30d2e6a1fceecebec496eb7a45f711
William <will2b@gmail.com>
2021-10-10 00:39:30 -0700

    edit header nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,74 @@
 import React from "react";
 import { ReactComponent as BagIcon } from "../images/bag.svg";
+import style from "./Header.module.css";
 
 const Header = () => {
   return (
     <header className="container mx-auto py-10 px-5">
-      <div className="flex justify-between items-center">
-        <nav>
-          <div className="hamburger-icon w-8 h-8 p-1 flex flex-col justify-around">
+      <nav className="w-full flex justify-between items-center">
+        <div className="left-nav w-full">
+          <div className="lg:hidden hamburger-icon w-8 h-8 p-1 flex flex-col justify-around">
             <span className="w-full h-1 block border-b border-dark"></span>
             <span className="w-full h-1 block border-b border-dark"></span>
             <span className="w-full h-1 block border-b border-dark"></span>
           </div>
-        </nav>
-        <a href="/" className="h-full flex flex-col text-center justify-center">
-          <h1 className="uppercase text-3xl leading-5">collllect</h1>
-          <h2 className="text-2xs">Mine & Yours</h2>
-        </a>
-        <div>
-          <div className="h-full flex items-center pb-0.5 hover:border-b hover:border-dark">
-            {" "}
-            <BagIcon className="w-7" />
-            <span className="ml-1 text-lg">0</span>
+          <div className="hidden lg:block w-full">
+            <ul className="flex space-x-8">
+              <li>
+                <a className={style.header__link_item} href="#">
+                  MAIN
+                </a>
+              </li>
+              <li>
+                <a className={style.header__link_item} href="#">
+                  SHOP
+                </a>
+              </li>
+              <li>
+                <a className={style.header__link_item} href="#">
+                  More
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-      </div>
+        <div className="w-1/3 px-16">
+          <a
+            href="/"
+            className="h-full flex flex-col text-center justify-center"
+          >
+            <h1 className="uppercase text-3xl md:text-4xl lg:text-5xl leading-5">
+              collllect
+            </h1>
+            <h2 className="text-2xs md:text-xs lg:text-sm">Mine & Yours</h2>
+          </a>
+        </div>
+
+        <div className="right-nav w-full">
+          <div className="h-ful flex">
+            <div className="hidden lg:block w-full pr-8">
+              <ul className="h-full flex justify-end items-center space-x-8">
+                <li>
+                  <a className={style.header__link_item} href="#">
+                    Log In
+                  </a>
+                </li>
+                <li>
+                  <a className={style.header__link_item} href="#">
+                    Search
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div
+              className={`${style.header__link_item} flex justify-end items-center pb-0.5`}
+            >
+              <BagIcon className="w-7" />
+              <span className="ml-1 text-lg">0</span>
+            </div>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 };
