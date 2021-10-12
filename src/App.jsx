b3caa3ec9758318de7e29d79f0cdc65e2f754f20
William <will2b@gmail.com>
2021-10-11 23:08:46 -0700

    add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Alert from "./components/Alert";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
-      <Home />
+      <Alert />
+      <Header />
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route exact path="/" component={Home} />
+      </Switch>
+      <Footer />
     </>
   );
 }
