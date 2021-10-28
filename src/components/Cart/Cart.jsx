d29cd0c2aa1af60515d1a2b1df65cdaa742f579f
William <will2b@gmail.com>
2021-10-27 22:55:23 -0700

    separate CartWrapper to a component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,49 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { cartActions } from "../../store/cart-slice";
+import { useSelector } from "react-redux";
 
 import clxs from "../../utils/clxs";
 
 import "../../index.css";
-import Product from "../../pages/Product";
 import CartItem from "./CartItem";
+import CartWrapper from "./CartWrapper";
 
 const CartElement = (props) => {
-  const dispatch = useDispatch();
+  console.log("props: ", props);
   const cartItems = useSelector((state) => state.cart.cartItems);
   const totalQuantity = useSelector((state) => state.cart.totalQuantity);
   const totalPrice = useSelector((state) => state.cart.totalPrice);
-
-  const toggleCartHandler = () => {
-    dispatch(cartActions.toggle());
-  };
-
-  const CartWrapper = ({ children }) => {
-    return (
-      <section
-        className={clxs(
-          "pb-10",
-          " bg-lightOrange-50 z-30",
-          "w-full min-h-max text-lightOrange-800 animate-fadeIn",
-          props.className,
-        )}
-      >
-        <div
-          onClick={toggleCartHandler}
-          className={clxs(
-            "w-8 h-8 absolute top-4 right-4 cursor-pointer",
-            "flex flex-col justify-center items-center",
-            "transform transition-all hover:rotate-90",
-          )}
-        >
-          <span className="absolute h-px w-full bg-lightOrange-800 rotate-45"></span>
-          <span className="absolute h-px w-full bg-lightOrange-800 -rotate-45"></span>
-        </div>
-        <div className="container mx-auto px-8 pt-20">{children}</div>
-      </section>
-    );
-  };
 
   const EmptyCart = () => {
     return (
