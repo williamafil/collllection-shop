d27b2ac3112dd623134e67c627851037ab259b95
William <will2b@gmail.com>
2021-11-03 15:06:30 -0700

    add login/logout check

--- src/components/Header/MobileNavOverlay.jsx
+++ src/components/Header/MobileNavOverlay.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { auth } from "../../firebase/firebase";
 import { uiActions } from "../../store/ui-slice";
 import { pathToLogin, pathToSignup } from "../../router";
+import { current } from "immer";
 
 const MobileNavOverlay = () => {
   const dispatch = useDispatch();
   const history = useHistory();
+  const currentUser = useSelector((state) => state.user.currentUser);
 
   const loginHandler = () => {
     history.push(pathToLogin);
@@ -37,16 +40,37 @@ const MobileNavOverlay = () => {
           </span>
         </li>
       </ul>
-      <ul className="mt-28 space-y-4 tracking-wide">
-        <li>
-          <span onClick={loginHandler} className="py-1 pr-2 cursor-pointer">
-            Log in
-          </span>
-        </li>
-        <li onClick={signupHandler} className="py-1 pr-2 cursor-pointer">
-          Create account
-        </li>
-      </ul>
+      {!currentUser ? (
+        <ul className="mt-28 space-y-4 tracking-wide">
+          <li>
+            <span onClick={loginHandler} className="py-1 pr-2 cursor-pointer">
+              Log in
+            </span>
+          </li>
+          <li onClick={signupHandler} className="py-1 pr-2 cursor-pointer">
+            Create account
+          </li>
+        </ul>
+      ) : (
+        <ul className="mt-28 space-y-4 tracking-wide">
+          {currentUser.firstName !== "" && (
+            <li>
+              <span className="py-1 pr-2">
+                Logged in as {currentUser.firstName}
+              </span>
+            </li>
+          )}
+
+          <li>
+            <span
+              onClick={() => auth.signOut()}
+              className="py-1 pr-2 cursor-pointer"
+            >
+              Log out
+            </span>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
