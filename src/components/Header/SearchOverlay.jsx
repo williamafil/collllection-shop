b8cd2e30b8acef4e252d7ad589ceb4ba5e2be9bc
William <will2b@gmail.com>
2021-11-05 14:06:51 -0700

    add search overlay layout

--- src/components/Header/SearchOverlay.jsx
+++ src/components/Header/SearchOverlay.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { ReactComponent as SearchIcon } from "../../images/magnifying-glass.svg";
 
 const SearchOverlay = () => {
-  return <div>search overlay</div>;
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex justify-center h-14 space-x-2"
+    >
+      <input
+        type="text"
+        ref={inputRef}
+        className=" w-1/2 py-2 border-b border-lightOrange-800 bg-transparent focus:outline-none text-2xl tracking-wide placeholder-lightOrange-800"
+        placeholder="Search"
+      />
+      <button>
+        <SearchIcon className="py-1 px-1 w-12 h-full" />
+      </button>
+    </form>
+  );
 };
 
 export default SearchOverlay;
