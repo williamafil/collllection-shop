8d5c647159ac3f71ec3d01013be2c5c74139b0b3
William <will2b@gmail.com>
2021-11-09 09:16:39 -0800

    Set built.minify to terser

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig({
   plugins: [react(), svgr()],
   build: {
+    minify: "terser",
     terserOptions: {
       compress: {
         // remove console in production
